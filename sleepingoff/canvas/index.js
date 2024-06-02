37b46b20f55eb073ad9bdb17ca70b128875b8c5c
Sleepingoff <justjh30@gmail.com>
2024-06-02 10:57:05 +0900

    캔버스 연습

---
+++ sleepingoff/canvas/index.js
@@ -0,0 +1,54 @@
+const canvas = document.querySelector("canvas");
+
+const ctx = canvas.getContext("2d");
+
+//canvas 크기
+//1. canvas 태그에 css 주기
+//2. canvas 태그에 width, height 주기
+
+const canvasWidth = innerWidth;
+const canvasHeight = innerHeight;
+
+// canvas.style.width = 400 + "px";
+// canvas.style.height = 400 + "px";
+//결과: 400, 400의 크기에 300/400 배 확대됨
+//즉, 캔버스의 실제 크기는 300 * 300 이나, css에 의해 크기를 확대 혹은 축소된다.
+
+// canvas.style.width = canvasWidth + "px";
+// canvas.style.height = canvasHeight + "px";
+// canvas.width = canvasWidth;
+// canvas.height = canvasHeight;
+
+canvas.style.backgroundColor = "#ccc";
+
+//dpr: device pixel ratio
+//사용되는 장치의 픽셀 수.
+
+//개인 dpr에 맞춰 픽셀 수 조절해주기 => 더 선명해지는 효과
+console.log(window.devicePixelRatio); //1
+const myDPR = window.devicePixelRatio;
+canvas.width = canvasWidth * myDPR;
+canvas.height = canvasHeight * myDPR;
+ctx.scale(myDPR, myDPR);
+
+//dpr=1
+//10, 10 좌표에 50, 50 크기의 사각형 그리기
+ctx.fillRect(10, 10, 50, 50);
+
+//dpr=2
+const Xdpr2 = 80;
+const Ydpr2 = 10;
+const rectWidth = 25;
+const rectHeight = 25;
+const rect1Ofdpr2 = { x: Xdpr2, y: Ydpr2 };
+const rect2Ofdpr2 = { x: Xdpr2 + rectWidth, y: Ydpr2 };
+const rect3Ofdpr2 = { x: Xdpr2, y: Ydpr2 + rectHeight };
+const rect4Ofdpr2 = { x: Xdpr2 + rectWidth, y: Ydpr2 + rectHeight };
+
+ctx.strokeRect(rect1Ofdpr2.x, rect1Ofdpr2.y, rectWidth, rectHeight);
+ctx.strokeRect(rect2Ofdpr2.x, rect2Ofdpr2.y, rectWidth, rectHeight);
+ctx.strokeRect(rect3Ofdpr2.x, rect3Ofdpr2.y, rectWidth, rectHeight);
+ctx.strokeRect(rect4Ofdpr2.x, rect4Ofdpr2.y, rectWidth, rectHeight);
+
+export { canvasWidth, canvasHeight };
+export default ctx;
---
+++ sleepingoff/canvas/particle.js
@@ -0,0 +1,67 @@
+import ctx, { canvasHeight, canvasWidth } from "./index.js";
+//path 그리기를 이용해서 파티클 생성하기
+class Particle {
+  constructor(x, y, radius, vy) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.vy = vy;
+  }
+  update() {
+    this.y += this.vy;
+  }
+  draw() {
+    ctx.beginPath();
+    //Math.PI / 180 = 1도
+    ctx.arc(this.x, this.y, this.radius, 0, (Math.PI / 180) * 360);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+const TOTAL = 10;
+const particles = [];
+
+const randomNumBetween = (min, max) => {
+  return Math.random() * (max - min + 1) + min;
+};
+
+for (let i = 0; i < TOTAL; i++) {
+  const x = randomNumBetween(0, canvasWidth);
+  const y = randomNumBetween(0, canvasHeight);
+  const radius = randomNumBetween(50, 100);
+  const vy = randomNumBetween(1, 5);
+  const particle = new Particle(x, y, radius, vy);
+  particles.push(particle);
+}
+
+let interval = 1000 / 60;
+let now, delta;
+let then = Date.now();
+
+const animate = () => {
+  window.requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  now = Date.now();
+  delta = now - then;
+
+  if (delta < interval) return;
+
+  particles.forEach((particle) => {
+    particle.update();
+
+    particle.draw();
+
+    if (particle.y - particle.radius > canvasHeight) {
+      particle.y = -particle.radius;
+      particle.x = randomNumBetween(0, canvasWidth);
+      particle.radius = randomNumBetween(50, 100);
+      particle.vy = randomNumBetween(1, 5);
+    }
+  });
+
+  then = now - (delta % interval);
+};
+
+animate();
