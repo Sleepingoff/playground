40f8e423a6e265fd2f9713d28d570474d515189b
Sleepingoff <justjh30@gmail.com>
2024-06-10 15:39:58 +0900

    인형뽑기 집게 완료

--- sleepingoff/matter/test.js
+++ sleepingoff/matter/test.js
@@ -1,35 +1,152 @@
-import { Composite, Constraint, engine, mouse } from "./index.js";
+import {
+  Body,
+  Composite,
+  Constraint,
+  Events,
+  canvasHeight,
+  engine,
+  mouse,
+  mouseConstraint,
+  render,
+  runner,
+} from "./index.js";
 import { createBar } from "./func.js";
+import { calcBarLocalPosition } from "./util.js";
+const composites = [];
 
-const bar = createBar(20, 30, 5, 50);
-const barConstraint = Constraint.create({
-  bodyA: bar,
-  pointB: { x: 20, y: 5 }, //bar의 x 좌표, bar의 상단 y 좌표 = 30 - 50 / 2
-  pointA: { x: 0, y: -25 }, //bar의 중심 x, y에서 y만큼 25 올라간 위치
-});
-//두 좌표간 y 좌표의 차이 20 === pointB와 pointA사이의 거리
-const bar_bar1 = createBar(20, 30 + 25 + 25 / 2, 5, 25);
-const bar1Constraint = Constraint.create({
-  bodyA: bar,
-  //bar에서 매달고 싶은 위치
-  pointA: { x: 0, y: 25 },
-  bodyB: bar_bar1,
-  //bar1에서 매달리고 싶은 위치
-  pointB: { x: 0, y: -25 / 2 },
+const BAR_INIT_LENGTH = 50;
+const BAR_INIT_WIDTH = 5;
+const BAR_INIT_ANGLE = 60;
+const BAR_SECOND_ANGLE = -90; //음수값으로 고정
+let isMove = false;
+let isMouseOn = false;
+let isDown = true;
+let isClick = true;
+function createChain(currentMousePosition, index, rads = []) {
+  const bar = createBar(
+    currentMousePosition,
+    BAR_INIT_LENGTH,
+    BAR_INIT_WIDTH,
+    BAR_INIT_LENGTH + index,
+    0,
+    {
+      render: {
+        fillStyle: "#fff",
+      },
+    }
+  );
+
+  const barConstraint = Constraint.create({
+    bodyA: bar,
+    pointB: { x: currentMousePosition, y: 0 },
+    pointA: { x: 0, y: -BAR_INIT_LENGTH / 2 },
+    length: 0,
+  });
+  //두 좌표간 y 좌표의 차이 20 === pointB와 pointA사이의 거리
+  const bar_left = createBar(
+    currentMousePosition,
+    BAR_INIT_LENGTH + BAR_INIT_LENGTH / 2,
+    BAR_INIT_WIDTH,
+    BAR_INIT_LENGTH / 2,
+    Math.PI / 2,
+    {
+      render: {
+        fillStyle: "#fff",
+      },
+    }
+  );
+  const localPosition = calcBarLocalPosition(25, rads[0]);
+
+  Body.rotate(bar_left, rads[0]);
+
+  const leftConstraint = Constraint.create({
+    pointA: localPosition,
+    bodyA: bar_left,
+    pointB: { x: currentMousePosition, y: 50 + index / 2 },
+    length: 0,
+  });
+  const bar_left_bar0 = createBar(
+    currentMousePosition,
+    2 * BAR_INIT_LENGTH + BAR_INIT_LENGTH / 4,
+    BAR_INIT_WIDTH,
+    BAR_INIT_LENGTH / 4,
+    Math.PI / 2 - rads[1],
+    {
+      render: {
+        fillStyle: "#fff",
+      },
+    }
+  );
+  Body.rotate(bar_left_bar0, -30);
+  const bar_left_bar0_localPosition = calcBarLocalPosition(
+    25,
+    Math.PI / 2 - rads[1]
+  );
+  const left_bar0Constraint = Constraint.create({
+    bodyA: bar_left,
+    pointA: { x: -localPosition.x, y: -localPosition.y },
+    bodyB: bar_left_bar0,
+    pointB: {
+      x: bar_left_bar0_localPosition.x,
+      y: bar_left_bar0_localPosition.y - index / 64,
+    },
+    length: 0,
+  });
+
+  composites.push(
+    bar,
+    barConstraint,
+    bar_left,
+    leftConstraint,
+    bar_left_bar0,
+    left_bar0Constraint
+  );
+}
+Events.on(mouseConstraint, "mousedown", () => {
+  isClick = !isMove;
+  if (isClick) isMove = true;
 });
 
-const bar_bar1_bar2 = createBar(20, 30 + 25 + 25 + 25 / 2 / 2, 5, 25 / 2);
-const bar2Constraint = Constraint.create({
-  bodyA: bar_bar1,
-  pointA: { x: 0, y: 25 / 2 },
-  bodyB: bar_bar1_bar2,
-  pointB: { x: 0, y: -25 / 2 / 2 },
+render.canvas.addEventListener("mouseover", (event) => {
+  isMouseOn = true;
 });
-Composite.add(engine.world, [
-  bar,
-  barConstraint,
-  bar_bar1,
-  bar1Constraint,
-  bar_bar1_bar2,
-  bar2Constraint,
-]);
+render.canvas.addEventListener("mouseleave", () => {
+  isMouseOn = false;
+});
+let index = 0;
+const DEG_TO_RAD = Math.PI / 180;
+Events.on(runner, "tick", () => {
+  const currentMousePosition = mouse.position.x;
+  const currentMousePositionY = mouse.position.y;
+  let ceilingBarLength = BAR_INIT_LENGTH + index;
+
+  const initialAngleRad = BAR_INIT_ANGLE * DEG_TO_RAD * 2;
+  const secondAngleRad =
+    BAR_SECOND_ANGLE * DEG_TO_RAD * 2 -
+    initialAngleRad +
+    index / (currentMousePositionY / 2); //initialAngle과 일직선보다 넘지 않게 조절
+
+  composites.forEach((comp) => {
+    Composite.remove(engine.world, comp);
+  });
+  composites.length = 0; // 배열 초기화
+
+  if (ceilingBarLength >= currentMousePositionY * 2) isDown = false;
+  if (ceilingBarLength < BAR_INIT_LENGTH) isDown = true;
+
+  createChain(currentMousePosition, index, [-initialAngleRad, secondAngleRad]);
+  createChain(currentMousePosition, index, [
+    Math.PI + initialAngleRad,
+    -secondAngleRad,
+  ]);
+
+  if (isMove) {
+    if (ceilingBarLength < BAR_INIT_LENGTH && isDown) {
+      index = BAR_INIT_LENGTH;
+      isMove = false;
+    }
+    index = isDown ? index + 2 : index - 2;
+  }
+
+  Composite.add(engine.world, composites);
+});
