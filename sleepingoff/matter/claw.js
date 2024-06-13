d8508e0339b9fcc3cc233cc48a09cea92b4c6fe2
Sleepingoff <justjh30@gmail.com>
2024-06-13 20:24:50 +0900

    동작 분리하기

--- sleepingoff/matter/claw.js
+++ sleepingoff/matter/claw.js
@@ -22,12 +22,68 @@ const BAR_SECOND_ANGLE = -90; //음수값으로 고정
 const composites = [];
 let isMove = false;
 let isMouseOn = false;
-let isDown = true;
-let isClick = true;
+let isDown = false;
+let isClick = false;
+
+let index = 0;
+
+//todo: 각각의 움직임 분리하기
+// 1. x 좌표 움직임
+let currentX = 100;
+let xTargetPosiiton = currentX + 200;
+//click -> currentMousePositionX와 비교 -> xMovement 이동
+function moveToX(target) {
+  const xMovement = currentX - target;
+  // 다 움직임.
+  if (xMovement === 0) return currentX;
+  //xMovement > 0 ? 왼쪽 이동 : 오른쪽 이동
+  currentX = xMovement > 0 ? currentX - 2 : currentX + 2;
+  if (xMovement === 1) currentX--;
+  return xMovement > 0 ? currentX - 2 : currentX + 2;
+}
+// 2. y 좌표 움직임
+//충돌 지점
+let currentY = 0;
+let yTargetPosition = canvasHeight;
+//기본적으로 current가 항상 낮은 값으로 취급.
+//즉, moveToY의 기본 동작은 down
+function moveToY(target) {
+  const yMovement = target - currentY;
+  // 다 움직임.
+  if (yMovement === 0) return currentY;
+  //yMovement > 0 ? 아래로 이동 : 위로 이동
+  currentY = yMovement > 0 ? currentY + 2 : currentY - 2;
+  if (yMovement === 1) currentY--;
+  return yMovement > 0 ? currentY + 2 : currentY - 2;
+}
+// 3. 각도 움직임
+const MAX_INIT_RAD = 90 * DEG_TO_RAD;
+const MIN_INIT_RAD = 0 * DEG_TO_RAD;
+
+const MAX_SECOND_RAD = -90 * DEG_TO_RAD;
+const MIN_SECOND_RAD = -180 * DEG_TO_RAD;
+//현재 y좌표 혹은 충돌 여부에 따라 동작
+//이 함수가 실행되면 다른 움직임은 취소
+let isRotateMinToMax = true;
+function rotateTo([currentInitRad, currentSecondRad]) {
+  //기본은 모두 min으로 설정
+  //min -> max -> min 순으로 움직임
+  if (isRotateMinToMax) {
+    //증감값은 추후 조정
+    currentInitRad < MAX_INIT_RAD && currentInitRad++;
+    currentSecondRad < MAX_SECOND_RAD && currentSecondRad--;
+  } else {
+    currentInitRad < MAX_INIT_RAD && currentInitRad--;
+    currentSecondRad < MAX_SECOND_RAD && currentSecondRad++;
+  }
+
+  return [currentInitRad, currentSecondRad];
+}
 
 Events.on(mouseConstraint, "mousedown", () => {
-  isClick = !isMove;
-  if (isClick) isMove = true;
+  isClick = !isClick;
+  xTargetPosiiton = mouse.position.x;
+  yTargetPosition = mouse.position.y;
 });
 
 render.canvas.addEventListener("mouseover", (event) => {
@@ -36,28 +92,27 @@ render.canvas.addEventListener("mouseover", (event) => {
 render.canvas.addEventListener("mouseleave", () => {
   isMouseOn = false;
 });
-let index = 0;
-//todo: 각각의 움직임 분리하기
-// 1. x 좌표 움직임
-// 2. y 좌표 움직임
-// 3. 각도 움직임
-Events.on(runner, "tick", () => {
-  const currentMousePosition = mouse.position.x;
-  const currentMousePositionY = mouse.position.y;
-  let ceilingBarLength = BAR_INIT_LENGTH + index;
 
-  const initialAngleRad =
-    BAR_INIT_ANGLE * DEG_TO_RAD - index / currentMousePositionY / 2;
-  const secondAngleRad =
-    BAR_SECOND_ANGLE * DEG_TO_RAD - index / currentMousePositionY; //initialAngle과 일직선보다 넘지 않게 조절
+Events.on(runner, "tick", () => {
+  const initialAngleRad = BAR_INIT_ANGLE * DEG_TO_RAD;
+  const secondAngleRad = BAR_SECOND_ANGLE * DEG_TO_RAD;
 
   composites.forEach((comp) => {
     Composite.remove(engine.world, comp);
   });
   composites.length = 0; // 배열 초기화
+  const currentMousePosition = moveToX(xTargetPosiiton);
+  if (currentMousePosition === currentX && isClick) {
+    //todo: isDown이 현재 boolean이나 rotate까지 들어가면 string형태로 확인하자.
+    if (!isDown && yTargetPosition != currentY) {
+      index = moveToY(yTargetPosition);
+      isDown = index > yTargetPosition;
+    }
+    if (isDown) index = moveToY(0);
+  } else {
+    isDown = false;
+  }
 
-  if (ceilingBarLength >= currentMousePositionY * 2) isDown = false;
-  if (ceilingBarLength < BAR_INIT_LENGTH) isDown = true;
   const bar = createBar(
     currentMousePosition,
     BAR_INIT_LENGTH,
@@ -84,14 +139,6 @@ Events.on(runner, "tick", () => {
     Math.PI + initialAngleRad,
     -secondAngleRad,
   ]);
-
-  if (isMove) {
-    if (ceilingBarLength < BAR_INIT_LENGTH && isDown) {
-      index = 0;
-      isMove = false;
-    }
-    index = isDown ? index + 2 : index - 2;
-  }
 
   Composite.add(engine.world, composites);
 });
